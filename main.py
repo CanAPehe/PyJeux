5513d2768575acc667c2e059f894fd785d9a014f
CanAPehe <68670251+CanAPehe@users.noreply.github.com>
2020-07-23 02:25:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-#Je voudrais faire bouger mon perssonnage sur l'axe -X
-
+#Import
 import pygame
 
 pygame.init()
 
+#Fenêtre du jeux
 ecran = pygame.display.set_mode((1250, 650))
 
 pygame.display.set_caption("PyJeux")
 
-continuer = True
 
 fond = pygame.image.load("bg.jpg").convert()
 
 ecran.blit(fond, (-10,-10))
 pygame.display.flip()
 
+#Joueur
 perso = pygame.image.load("perso.png").convert_alpha()
 
 ecran.blit(perso, (0,440))
 pygame.display.flip()
+
+#Boucle
+continuer = True
 
 while continuer:
     for event in pygame.event.get():
